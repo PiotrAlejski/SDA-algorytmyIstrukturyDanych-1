a2d81a5d455efac1d48093bf6d5636922dba34bc
Marta <muszkamarta@interia.pl>
2017-02-14 18:08:43 +0100

    Initial commit. Application

---
+++ src/algorytmy/Application.java
@@ -0,0 +1,10 @@
+package algorytmy;
+
+/**
+ * Created by RENT on 2017-02-14.
+ */
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
