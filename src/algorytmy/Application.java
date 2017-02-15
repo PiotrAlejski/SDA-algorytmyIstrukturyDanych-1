f83e04a9bbc2cc7a2ef0b14c8349bf7488df5c6c
Marta <muszkamarta@interia.pl>
2017-02-15 21:02:08 +0100

    Added methods: put, add with index, delete, toString

--- src/algorytmy/Application.java
+++ src/algorytmy/Application.java
@@ -5,9 +5,40 @@ import algorytmy.structures.MyList;
 public class Application {
     public static void main(String[] args) {
 
-
 //        do MyList:
 
+        MyList myList = initMyList();
+        MyList clonedList = myList.clone();
+
+        System.out.println(myList);
+        System.out.println(clonedList);
+
+        clonedList.add(5);
+
+        System.out.println(myList);
+        System.out.println(clonedList);
+
+        clonedList.delete(3);
+
+        System.out.println(myList);
+        System.out.println(clonedList);
+
+        System.out.println();
+
+        clonedList.add(3, 333);
+
+        System.out.println(myList);
+        System.out.println(clonedList);
+
+        System.out.println();
+
+        clonedList.put(3, 555);
+
+        System.out.println(myList);
+        System.out.println(clonedList);
+    }
+
+    private static MyList initMyList() {
         MyList myList = new MyList();
         myList.add(2);
         myList.add(3);
@@ -21,7 +52,7 @@ public class Application {
         myList.add(-1);
         myList.add(0);
         myList.add(2);
-        System.out.println(myList.get(11));
-
+        myList.add(-2);
+        return myList;
     }
 }
--- src/algorytmy/structures/MyList.java
+++ src/algorytmy/structures/MyList.java
@@ -14,16 +14,49 @@ public class MyList {
         this.size = 0;
     }
 
+//    nadpisujemy istniejacy element:
+
+    public void put(int index, int value) {
+        if(checkIndex(index)) {
+            this.array[index] = value;
+        }
+    }
+
+//    dodajemy element po istniejacym indeksem, zwiekszamy size
+//    nie na koncu
+
+    public void add(int index, int value) {
+        if(checkIndex(index)) {
+            if (this.size >= this.array.length) {
+                doubleCapacity();
+            }
+
+//            przejscie od konca listy, petla "od tylu"
+
+            for (int i = this.size; i > index ; i--) {
+                this.array[i] = this.array[i - 1];
+            }
+            this.size++;
+            this.array[index] = value;
+        }
+    }
+
 //    metoda spr czy indeks jest poprawny
 
     public int get(int index) {
-        if(index >= 0 && index < size) {
+        if(checkIndex(index)) {
             return array[index];
         } else {
             System.out.println("Wrong index");
             return 0;
         }
 
+    }
+
+//  ctrl+alt+m z zaznaczonego fragmentu tworzymy nowa metode
+
+    private boolean checkIndex(int index) {
+        return index >= 0 && index < size;
     }
 
 //    dodawanie elementu
@@ -48,6 +81,24 @@ public class MyList {
             size++;
     }
 
+    public void delete(int index) {
+        if(checkIndex(index)) {
+            for (int i = index; i < size - 1; i++) {
+
+//                swap(i, i + 1 ); - lepsze rozwiazanie ponizej
+
+                array[i] = array[i + 1];
+            }
+            size--;
+        }
+    }
+
+    private void swap(int i1, int i2) {
+        int tmp = this.array[i1];
+        this.array[i1] = this.array[i2];
+        this.array[i2] = tmp;
+    }
+
     public MyList clone() {
         MyList tmpList = new MyList();
         tmpList.array = rewrite(new int[this.array.length]);
@@ -66,12 +117,25 @@ public class MyList {
 
     private int[] rewrite(int[] tmpArray) {
         for (int i = 0; i < array.length; i++) {
-            this.array[i] = tmpArray[i];
+             tmpArray[i] = this.array[i];
         }
         return tmpArray;
     }
 
     public int getSize() {
         return size;
+    }
+
+    public String toString() {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("[");
+        for (int i = 0; i < this.size; i++) {
+            stringBuilder.append(array[i]);
+            if (i != size - 1) {
+                stringBuilder.append(",");
+            }
+        }
+        stringBuilder.append("]");
+        return stringBuilder.toString();
     }
 }
